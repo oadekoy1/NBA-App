352aadb32f56654888aa74c65ffa5a6fbd57dd01
Oluwaseun Adekoya <oadekoy1@gmail.com>
2022-11-14 14:20:35 -0500

    NBA NEW Update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 // middleware
 app.use(morgan('dev'))
 app.use(express.json())
-// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(cors({
     origin: "*",
@@ -25,15 +25,6 @@ app.use(require('./config/checkToken'));
 
 // API Routes
 app.use('/api/users', require('./routes/api/users'))
-
-router.get('/', (req, res, next) => {
-    axios.get(`http://data.nba.net/prod/v1/current/standings_conference.json`)
-    then(response => {
-        res.send(response.data.result)
-    }).catch(error => {
-        res.send(error.message)
-    })
-})
 
 // Catch All to serve the production app
 app.get('/*', (req, res) => {
